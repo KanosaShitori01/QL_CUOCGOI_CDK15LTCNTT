2823f1329dcb9d958cc829e485897e80b7a7fbbc
SitorTasma <100101949+SitorTasma@users.noreply.github.com>
2022-03-21 08:12:11 +0700

    OKE

---
+++ qlcuocgoi_winform/qlcuocgoi_dtt/CNTTCG.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace qlcuocgoi_dtt
+{
+    partial class CNTTCG
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_giobatdau = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_gioketthuc = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_them = new System.Windows.Forms.Button();
+            this.cb_sothuebaogoi = new System.Windows.Forms.ComboBox();
+            this.cb_sothuebao = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(339, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CẬP NHẬT THÔNG TIN CUỘC GỌI";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(13, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Số thuê bao";
+            // 
+            // txt_giobatdau
+            // 
+            this.txt_giobatdau.Location = new System.Drawing.Point(105, 82);
+            this.txt_giobatdau.Name = "txt_giobatdau";
+            this.txt_giobatdau.Size = new System.Drawing.Size(279, 20);
+            this.txt_giobatdau.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(13, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Giờ bắt đầu";
+            // 
+            // txt_gioketthuc
+            // 
+            this.txt_gioketthuc.Location = new System.Drawing.Point(105, 108);
+            this.txt_gioketthuc.Name = "txt_gioketthuc";
+            this.txt_gioketthuc.Size = new System.Drawing.Size(279, 20);
+            this.txt_gioketthuc.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(13, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Giờ kết thúc";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label5.Location = new System.Drawing.Point(13, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Số thuê bao gọi";
+            // 
+            // btn_them
+            // 
+            this.btn_them.Location = new System.Drawing.Point(276, 177);
+            this.btn_them.Name = "btn_them";
+            this.btn_them.Size = new System.Drawing.Size(108, 30);
+            this.btn_them.TabIndex = 9;
+            this.btn_them.Text = "Thêm";
+            this.btn_them.UseVisualStyleBackColor = true;
+            // 
+            // cb_sothuebaogoi
+            // 
+            this.cb_sothuebaogoi.FormattingEnabled = true;
+            this.cb_sothuebaogoi.Location = new System.Drawing.Point(105, 134);
+            this.cb_sothuebaogoi.Name = "cb_sothuebaogoi";
+            this.cb_sothuebaogoi.Size = new System.Drawing.Size(279, 21);
+            this.cb_sothuebaogoi.TabIndex = 10;
+            // 
+            // cb_sothuebao
+            // 
+            this.cb_sothuebao.FormattingEnabled = true;
+            this.cb_sothuebao.Location = new System.Drawing.Point(105, 56);
+            this.cb_sothuebao.Name = "cb_sothuebao";
+            this.cb_sothuebao.Size = new System.Drawing.Size(279, 21);
+            this.cb_sothuebao.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 229);
+            this.Controls.Add(this.cb_sothuebao);
+            this.Controls.Add(this.cb_sothuebaogoi);
+            this.Controls.Add(this.btn_them);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_gioketthuc);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_giobatdau);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_giobatdau;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_gioketthuc;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_them;
+        private System.Windows.Forms.ComboBox cb_sothuebaogoi;
+        private System.Windows.Forms.ComboBox cb_sothuebao;
+    }
+}
+
---
+++ qlcuocgoi_winform/qlcuocgoi_dtt/DSKH.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace qlcuocgoi_dtt
+{
+    public partial class DSKH : Form
+    {
+        public DSKH()
+        {
+            InitializeComponent();
+        }
+    }
+}
